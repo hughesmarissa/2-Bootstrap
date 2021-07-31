177963c472e3af82494f8dadf712a5009698d716
Marissa Hughes <hughes.marissa331@gmail.com>
2021-07-31 10:27:43 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/* CAROUSEL FUNCTIONALITY */
 $(function() {
     $(".carousel").carousel( { interval: 2000 } );
     $("#carouselButton").click(function(){
@@ -10,5 +11,18 @@ $(function() {
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
+    });
+});
+
+/* BUTTONS FOR RESERVE&LOGIN MODALS */
+$(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+});
+
+$(function(){
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
     });
 });
